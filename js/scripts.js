3222e196fdb4d55c5ffb21e0dead34504cf83d3d
Esther <esmuthoni@gmail.com>
2017-08-26 13:11:12 +0300

    css and js updated. Added js functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,53 +1,121 @@
-function Game() {
-  this.roll=0;
-  this.subtotal= 0;
-  this.bank = 0;
-  this.strikes = 0
-}
-var UIgame = new Game ()
+var player1 = "";
+var player2 = "";
 
-Game.prototype.diceRoll = function () {
-  rollValue = Math.ceil((Math.random()*15));
-  this.roll = rollValue;
-  this.addToSubTotal();
+
+var throwdice = function() {
+  return Math.floor(6 * Math.random()) + 1;
 }
 
-Game.prototype.addToSubTotal = function () {
-    if (this.roll % 4 === 0) {
-    this.subtotal = 0;
-    this.strikes += 1
-  }
-  else {
-    this.subtotal += this.roll;
-    this.strikes = 0;
-  }
-  if (this.strikes === 3) {
-    UIgame.bank = 0;
-    UIgame.strikes = 0;
-    debugger;
+function Player(turn) {
+  this.roll = 0;
+  this.tempscore = 0;
+  this.totalscore = 0;
+  this.turn = turn;
+  this.playerName;
+}
+
+// checking for a winner and if the dice roll is 1
+Player.prototype.diceOne = function() {
+  if (this.roll === 1) {
+    this.tempscore = 0;
+    alert("OOPS! " + this.playerName + ",  your dice rolled to 1! Your turn is over!")
+  } else {
+    this.tempscore += this.roll;
   }
 }
 
-Game.prototype.bankedTotal = function () {
+// hold
+Player.prototype.hold = function() {
+  this.totalscore += this.tempscore;
+  this.tempscore = 0;
+  alert(this.playerName + ", Your turn is over!Next");
+}
 
-  this.bank += this.subtotal;
-  this.subtotal = 0;
-
-  if (this.bank >= 100) {
-    alert('You won!')
+// check for 100
+Player.prototype.checkWinner = function() {
+  if (this.totalscore >= 100) {
+    alert(this.playerName + " You are the winner!");
   }
 }
 
-$(document).ready(function(){
-  $('#roll').click(function(){
-    UIgame.diceRoll();
-    $('.rollNumber').text(UIgame.roll);
-    $('.subtotal').text(UIgame.subtotal);
-    $('.strike').text(UIgame.strikes);
-    $('.bank').text(UIgame.bank);
-  })
-  $('#keep').click(function(){
-    UIgame.bankedTotal();
-    $('.bank').text(UIgame.bank);
+Player.prototype.newGame = function() {
+  this.roll = 0;
+  this.tempscore = 0;
+  this.totalscore = 0;
+  this.playerName = "";
+}
+
+var resetFields = function() {
+  $(".player1").val("");
+  $(".player2").val("");
+}
+
+// User Interface
+$(document).ready(function() {
+  $("button#start").click(function(event) {
+    player1 = new Player(true);
+    player2 = new Player(false);
+    $(".display").show();
+    $(".names").hide();
+
   });
+  $("form#player").submit(function(event) {
+    event.preventDefault();
+    var player1Name = $("input.player1").val();
+    console.log(player1Name);
+    $("#player1").text(player1Name);
+
+    var player2Name = $(".player2").val();
+    $("#player2").text(player2Name);
+
+    player1.playerName = player1Name;
+    player2.playerName = player2Name;
+  });
+
+  $("button#new-game").click(function(event) {
+    $(".display").hide();
+    clearValues();
+    player1.newGame();
+    player2.newGame();
+    $("#round-total-1").empty();
+    $("#total-score-1").empty();
+    $("#die-roll-1").empty();
+    $("#round-total-2").empty();
+    $("#total-score-2").empty();
+    $("#die-roll-2").empty();
+
+    $(".names").show();
+  });
+
+  $("button#player1-roll").click(function(event) {
+    player1.roll = throwdice();
+    $("#die-roll-1").text(player1.roll);
+    player1.diceOne();
+    $("#round-total-1").text(player1.tempscore);
+  });
+
+  $("button#player2-roll").click(function(event) {
+    player2.roll = throwdice();
+    $("#die-roll-2").text(player2.roll);
+    player2.diceOne();
+    $("#round-total-2").text(player2.tempscore);
+  });
+
+  $("button#player1-hold").click(function(event) {
+    player1.hold();
+    $("#total-score-1").text(player1.totalscore);
+    $("#round-total-1").empty();
+    $("#die-roll-1").empty();
+    player1.winnerCheck();
+  });
+
+  $("button#player2-hold").click(function(event) {
+    player2.hold();
+    $("#total-score-2").text(player2.totalscore);
+    $("#round-total-2").empty();
+    $("#die-roll-2").empty();
+    player2.winnerCheck();
+  });
+
+
 });
